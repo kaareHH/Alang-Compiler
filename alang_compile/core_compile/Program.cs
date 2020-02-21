ae93c1c685fa4a67b5acc6a1d2cc85ccf4b4c70b
AndersSpringborg <aaspringborg@gmail.com>
2020-02-21 13:03:41 +0100

    boiler plate fix

---
+++ alang_compile/core_compile/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace core_compile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
