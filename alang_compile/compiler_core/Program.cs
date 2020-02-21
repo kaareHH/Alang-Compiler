3c3c7dbfb3d5cc8dd25d46e81121bf913d276823
AndersSpringborg <aaspringborg@gmail.com>
2020-02-21 09:31:03 +0100

    boilerplate

---
+++ alang_compile/compiler_core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace compiler_core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
