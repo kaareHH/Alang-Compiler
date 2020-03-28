656f0d1b5e28166509e3b4afc1f4dbf02f0fa687
Kaare Hattel <kaare.hattel@gmail.com>
2020-03-28 17:07:15 +0100

    Parser still not working

--- alang_compile/core_compile/Parser.cs
+++ alang_compile/core_compile/Parser.cs
@@ -1,17 +1,21 @@
+using System.Runtime.ConstrainedExecution;
 using System.Linq;
 using System.Collections.Generic;
+using System;
 
 namespace core_compile
 {
     public class Parser
     {
         List<Token> tokenStream;
-        Token token;
+        Token currentToken;
+        Token prevToken;
 
         public Parser(List<Token> _tokenStream)
         {
             this.tokenStream = _tokenStream;
-            this.token = tokenStream.FirstOrDefault();
+            this.currentToken = tokenStream.FirstOrDefault();
+            Parse(currentToken);
         }
 
         /// <summary>
@@ -22,6 +26,9 @@ namespace core_compile
             // Get the current token
             Token currentToken = token;
 
+            // Set the previous token
+            prevToken = token;
+
             // Remove the current token to get the next
             tokenStream.Remove(token);
 
@@ -30,37 +37,157 @@ namespace core_compile
 
         }
 
-        public void Parse() 
+        public void Parse(Token token) 
         {
             if(token.TokenType == TokenType.T_INTDCL || token.TokenType == TokenType.T_PINDCL)
             {
-                Dcls();
+                Dcls(token);
             }
-        }
-
-        private void Dcls()
-        {
-            Dcl();
-            Dcls();
-
-        }
-
-        private Node Dcl()
-        {
-            if(token.TokenType == TokenType.T_INTDCL)
+            else 
             {
-                return MakeLeaf(token.TokenValue, ConvertType(token.TokenType));
+                System.Console.WriteLine("Done reading the tokens");
             }
-
-            return null;
 
         }
 
-        private NodeType ConvertType(TokenType tokenType)
+        private void Dcls(Token token)
         {
-            NodeType nodeType;
+            Node node;
+            node = Dcl(token);
 
-            switch(tokenType)
+            System.Console.WriteLine(node.type);
+            System.Console.WriteLine(node.left.type);
+            System.Console.WriteLine(node.right.type);
+
+
+            //Dcls();
+
+
+
+        }
+
+        private Node Dcl(Token token)
+        {
+            prevToken = token;
+            if(token.TokenType == TokenType.T_INTDCL || token.TokenType == TokenType.T_PINDCL)
+            {
+                if(Expect(token, TokenType.T_ID))
+                {
+
+                    token = GetNext(token);
+
+                    return MakeNode(token.TokenValue, 
+                            MakeLeaf(prevToken.TokenValue, ConvertType(prevToken)), 
+                            EvalRight(token), 
+                            ConvertType(token));
+                }
+                else 
+                {
+                    System.Console.WriteLine("Invalid next token");
+                    return null;
+                }
+
+            }
+            else 
+            {
+                System.Console.WriteLine("Invalid token");
+                return null;
+            }
+            
+
+        }
+
+        private bool Expect(Token token, TokenType type)
+        {
+            Token nextToken = GetNext(token);
+
+            if(nextToken.TokenType == type)
+                return true;
+
+            else
+                return false;
+        }
+
+        private Node EvalRight(Token token)
+        {
+            if(Expect(token, TokenType.T_EQUAL))
+            {
+                token = GetNext(token);
+
+                if(Expect(token, TokenType.T_ID) || Expect(token, TokenType.T_INTLIT))
+                {
+                    token = GetNext(token);
+                    if(Expect(token, TokenType.T_SEMICOLON)) // If the right side is finished
+                    {
+                        return MakeLeaf(token.TokenValue, ConvertType(token));
+                    }
+                    else if(Expect(token, TokenType.T_PLUS) || Expect(token, TokenType.T_MINUS) || Expect(token, TokenType.T_MULTIPLY) || Expect(token, TokenType.T_DIVIDE) )
+                    {
+                        Token prevToken = token;
+                        token = GetNext(token);
+                        return MakeNode(token.TokenValue, MakeLeaf(prevToken.TokenValue, ConvertType(prevToken)), SomeMethod(token), ConvertType(token));
+                        // Do something iterative - If we hit this case, we are encountering an arthmetic operation
+                    }
+                    else 
+                    {
+                        System.Console.WriteLine("Expected either an arthmetic operation or a semicolon after " + token.TokenType);
+                        return null;
+                    }
+                   
+                }
+                else
+                {
+                    System.Console.WriteLine("Expected ID or INTLIT after " + token.TokenType);
+                    return null;
+                }
+
+            }
+            else 
+            {
+                System.Console.WriteLine("Expected EQUAL after " + token.TokenType);
+                return null;
+            }
+            
+            
+        }
+
+        private Node SomeMethod(Token token)
+        {
+            // Recieves an operator (+ - * /)
+            // Fetches next token (ID or INTLIT)
+            token = GetNext(token);
+            // Checks if the token after the INT or ID is another operator.
+            // If it is, then create a Node and call this method for the right child.
+            if(Expect(token, TokenType.T_PLUS) || Expect(token, TokenType.T_MINUS) || 
+               Expect(token, TokenType.T_MULTIPLY) || Expect(token, TokenType.T_DIVIDE)) 
+            {
+                Token prevToken = token;
+                token = GetNext(token);
+
+                return MakeNode(token.TokenValue, MakeLeaf(prevToken.TokenValue, ConvertType(prevToken)), SomeMethod(token), ConvertType(token));
+
+            }
+            // If it is a semicolon, create a leaf
+            else if(Expect(token, TokenType.T_SEMICOLON))
+            {
+                return MakeLeaf(token.TokenValue, ConvertType(token));                
+            }
+            // Otherwise error
+            else 
+            {
+                System.Console.WriteLine("Expected either a SEMICOLON or an operator after " + token.TokenType);
+                return null;            
+            }
+
+            
+        }
+
+        private NodeType ConvertType(Token token)
+        {
+            NodeType nodeType = NodeType.N_BADNODE;
+            
+
+            switch(token.TokenType)
             {
                 case TokenType.T_INTDCL:
                 nodeType = NodeType.N_INTDCL;
@@ -68,6 +195,18 @@ namespace core_compile
 
                 case TokenType.T_PINDCL:
                 nodeType = NodeType.N_PINDCL;
+                break;
+
+                case TokenType.T_ID:
+                nodeType = NodeType.N_ID;
+                break;
+
+                case TokenType.T_INTLIT:
+                nodeType = NodeType.N_INTLIT;
+                break;
+
+                case TokenType.T_PLUS:
+                nodeType = NodeType.N_PLUS;
                 break;
 
                 default:
--- alang_compile/core_compile/Program.cs
+++ alang_compile/core_compile/Program.cs
@@ -7,21 +7,27 @@ namespace core_compile
     {
         static void Main(string[] args)
         {
-            var ged = Environment.CurrentDirectory;
-            string directory = ged.Remove(ged.IndexOf("bin"), ged.Length - ged.IndexOf("bin")) + "inputFile.txt";
+            //var ged = Environment.CurrentDirectory;
+            //string directory = ged.Remove(ged.IndexOf("bin"), ged.Length - ged.IndexOf("bin")) + "inputFile.txt";
 
-            char[] inputFile = System.IO.File.ReadAllText(directory).ToCharArray();
+            char[] inputFile = System.IO.File.ReadAllText(@"C:\Users\kaare\Documents\GitHub\p4-compiler\alang_compile\core_compile\inputFile.txt").ToCharArray();
 
             List<Token> tokens = new List<Token>();
 
-            Scanner scanner = new Scanner(inputFile);         
+            Scanner scanner = new Scanner(inputFile);
 
             tokens = scanner.tokens;
+
+            tokens.RemoveAll(x => (x.TokenType == TokenType.T_WHITESPACE));
 
             foreach (var item in tokens)
             {
                 Console.WriteLine(item.TokenType.ToString());
             }
+
+            Parser parser = new Parser(tokens);         
+
+            
         }
     }
 }
